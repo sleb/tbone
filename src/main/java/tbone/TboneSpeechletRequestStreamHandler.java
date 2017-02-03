49c915d2fea17c4af6d59de47b27e435dc91b68d
Scott LeBaron <lebaron@amaozon.com>
2017-02-02 22:45:20 -0800

    added debug logging

--- src/main/java/tbone/TboneSpeechletRequestStreamHandler.java
+++ src/main/java/tbone/TboneSpeechletRequestStreamHandler.java
@@ -1,13 +1,18 @@
 package tbone;
 
 import com.google.common.collect.ImmutableSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 
 public class TboneSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
     public TboneSpeechletRequestStreamHandler() {
         super(new TboneSpeechlet(), APPLICATION_IDS);
+        log.info("initialized speechlet with {}", APPLICATION_IDS);
     }
 
     private static final ImmutableSet<String> APPLICATION_IDS = ImmutableSet.of("amzn1.ask.skill.7df230ef-4a94-43b4-86cc-6b347a5a3a42"); // Test
+
+    private static final Logger log = LoggerFactory.getLogger(TboneSpeechletRequestStreamHandler.class);
 }
